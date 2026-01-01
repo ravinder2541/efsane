11e4296d4f02d951cec85814a404fc757474fc5a
kumar <kumardevloper22@gmail.com>
2026-01-01 11:48:28 +0530

    port changed in confirm code

--- src/app/api/reservation/confirm/route.ts
+++ src/app/api/reservation/confirm/route.ts
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   // port: parseInt(process.env.SMTP_PORT || '587'),
   port: parseInt(process.env.SMTP_PORT || '465'),
-  secure: process.env.SMTP_SECURE === 'true',
+  secure: process.env.SMTP_PORT === 'true',
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
